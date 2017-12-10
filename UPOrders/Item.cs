63d11924db223795bf22ffa2435a08bcbc48d4f1
Brecht Collen <collenbrecht@hotmail.com>
2017-12-10 14:25:14 +0100

    Add project files.

---
+++ UPOrders/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UPOrders
+{
+    public class Item
+    {
+        public Guid ItemId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
